10b21e6a84ae903b8a50622387454a49aa287414
Vasil Ch <vchaparov88@gmail.com>
2025-01-10 00:06:30 +0200

    Changes App.jsx location

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {Routes, Route} from "react-router";
+import Home from "./components/Pages/Home.jsx";
+import About from "./components/Pages/About.jsx";
+import Contacts from "./components/Pages/Contacts.jsx";
+import Shoes from "./components/Pages/Shoes.jsx";
+import Clothes from "./components/Pages/Clothes.jsx";
+import Swimsuits from "./components/Pages/Swimsuits.jsx";
+import './styles.css';
+import Layout from "./components/Layout.jsx";
+
+function App() {
+  return (
+      <Layout />
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from "./components/App";
+import App from "./App";
 
 const root = document.getElementById("root");
 
