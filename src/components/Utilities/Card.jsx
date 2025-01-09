d350b52d22406682674da98afe88129a7e690e8b
Vasil Ch <vchaparov88@gmail.com>
2025-01-10 01:08:06 +0200

    Change img url to use env baseurl

--- src/components/Utilities/Card.jsx
+++ src/components/Utilities/Card.jsx
@@ -4,7 +4,7 @@ function Card(props) {
   return (
 
     <div className="card">
-      <img className="card-image" src={`./src/assets/images/${props.imgURL}.jpg`}></img>
+      <img className="card-image" src={`${import.meta.env.BASE_URL}src/assets/images/${props.imgURL}.jpg`}></img>
       <div className="name">{props.name}</div>
     </div>
   );
