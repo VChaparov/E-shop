bbd43fa0638266820ded45cab382885e4cc9eaf6
Vasil Ch <vchaparov88@gmail.com>
2025-01-14 12:05:10 +0200

    Added mui to libraries, more general styling and searchbox ui behavior added

--- src/components/Pages/Header/SearchBar.jsx
+++ src/components/Pages/Header/SearchBar.jsx
@@ -1,7 +1,27 @@
+import { dropdownShow, dropdownHide } from "../../Utilities/dropdownToggle";
+
 function SearchBar() {
   return (
-    <div className="search-bar">
-      <input type="text"></input>
+    <div className="search-box">
+      <input
+        onFocus={() => {
+          dropdownShow();
+        }}
+        onBlur={() => dropdownHide()}
+        id="searchBar"
+        type="text"
+      ></input> 
+      <div id="search-dropdown-container">
+        
+        <ul id="search-dropdown">
+        <br/>
+          <li>item1</li>
+          <li>item1</li>
+          <li>item1</li>
+          <li>item1</li>
+          <li>item1</li>
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/components/Utilities/dropdownToggle.js
@@ -0,0 +1,13 @@
+
+
+
+export function dropdownShow() {
+  const searchDropdown = document.getElementById('search-dropdown-container');
+
+  searchDropdown.style.display = "flex";
+}
+export function dropdownHide() {
+  const searchDropdown = document.getElementById('search-dropdown-container');
+
+  searchDropdown.style.display = "none";
+}
